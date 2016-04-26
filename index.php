0247ef7232564fe30f173e18b3c966ec7f4a2a82
Your Name <ccsjgs1990@gmail.com>
2016-04-26 11:03:44 -0300

    index,insertar, style.css

--- index.php
+++ index.php
@@ -2,84 +2,42 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-  
   <title>IMDb.dit</title>
-  
   <link rel="shortcut icon" href="http://www.dit.ing.unp.edu.ar/wp-content/uploads/favicon.png" type="image/x-icon" />
   <link rel="stylesheet" type="text/css" href="content/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="content/css/style.css"media="screen" />
   <link rel="stylesheet" type="text/css" href="content/css/slidedvm.min.css"media="screen" />
-
   <script type="text/javascript" src="content/js/jquery-2.2.3.min.js" ></script>
   <script type="text/javascript" src="content/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="content/js/buscar.js"></script>
 
 </head>
-
 <body>
-  <div id="marco">
-
-    <div id="cabeza">
-      <div id="logo">
-        <h1>
-          <a href=" http://www.dit.ing.unp.edu.ar/">Acceso al Dit</a>
-        </h1> 
-      </div>
+  <div class="marco">
+    <div class="logo">
+        <h1><a href=" http://www.dit.ing.unp.edu.ar/"></a></h1> 
     </div>
-
-    <div id="toppic">
-      <div id="topwrapper">
-        <div id="buscadormenu">
+    <div class="toppic">
+        <div class="buscadormenu">
           <form id="searchform">
-            <input type="text" value="Buscar pelicula" name="id" id="id" onfocus="if (this.value == 'Buscar pelicula') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Buscar pelicula';}"><input type="image" src="http://3.bp.blogspot.com/-zq0urLoOrUk/ToqiHUJxaqI/AAAAAAAAC9I/ZP8dcRolI0w/s00/lupa.png" id="searchsubmit">
+             <input type="text" value="Buscar pelicula" name="id" id="id" onfocus="if (this.value == 'Buscar pelicula') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Buscar pelicula';}">
+             <input type="image" src="http://3.bp.blogspot.com/-zq0urLoOrUk/ToqiHUJxaqI/AAAAAAAAC9I/ZP8dcRolI0w/s00/lupa.png" id="searchsubmit">
+           </form>
+        </div>   
+        <div class="botonAgregar">
+          <a href="forminserta.html">Agregar pel&iacute;culas</a>
         </div>
-        <div class="link_catv2"><a href="forminserta.html">Agregar pel&iacute;culas</a>
+        <div class="botonCompara">
+          <a href="">Comparar</a>
         </div>
-      </div>
-      
-    </div><br>
-  
-    <div id="peliculas" class="row">
-      <div class="col-sm-3">
-
-      <!--
-      <li>
-        <a title="Batman - El caballero de la noche" href ="http://www.imdb.com/title/tt0468569">
-        <img class="panel" src="imagenes/1.jpg" alt="Batman - El caballero de la noche"/></a>
-      </li>
-
-      <li>
-        <a title="La caza al Octubre Rojo" href ="http://www.imdb.com/title/tt0099810">
-        <img class="panel" src="imagenes/2.jpg" alt="La caza al Octubre Rojo"/></a>
-      </li> 
-      
-      <li>
-         <a title="GoldenEye" href ="http://www.imdb.com/title/tt0113189">
-        <img class="panel" src="imagenes/3.jpg" alt="GoldenEye"/></a>
-      </li> 
-      <li>
-        <a title="American Pie" href ="http://www.imdb.com/title/tt0163651">
-        <img class="panel" src="imagenes/4.jpg" alt="American Pie"/></a>
-      </li> 
-      <li>
-        <a title="El silencio de los inocentes" href ="http://www.imdb.com/title/tt0102926/">
-        <img class="panel" src="imagenes/5.jpg" alt="El silencio de los inocentes"/></a>
-      </li>
-      <li>
-        <a title="El barco" href ="http://www.imdb.com/title/tt0082096">
-        <img class="panel" src="imagenes/6.jpg" alt="El barco"/></a>
-      </li>
-      <li>
-        <a title="Mujer bonita" href ="http://www.imdb.com/title/tt0100405">
-        <img class="panel" src="imagenes/7.jpg" alt="Mujer bonita"/></a>      
-      </li>
-    </ul> 
     </div>
-    <a href="#" class="slider-arrow sa-left"></a>
-    <a href="#" class="slider-arrow sa-right"></a>
-    -->
+<div id="peliculas" class="row">
+  <div class="col-sm-3"> </div>
 
-    </div>
-
+<div class="footer">
+  <p>* Diego Carabajal - Carla Santos *</p>
+</div>
 </body>
+
+
 </html>
